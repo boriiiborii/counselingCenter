12b449d68e4334fcf0f7bef6e008863e1829933f
Haena <ghkwlsspdl@naver.com>
2023-12-22 12:47:28 +0900

    [#001] FEAT: 버튼까지 애니메이팅
    - 레이블1, 레이블2, 로그인 버튼까지 애니메이팅 완료
    - 반복문 사용

--- counselingCenter/counselingCenter/loginPage/loginController.swift
+++ counselingCenter/counselingCenter/loginPage/loginController.swift
@@ -11,6 +11,7 @@ class loginController: UIViewController {
     
     @IBOutlet weak var welcomeLabel1: UILabel!
     @IBOutlet weak var welcomeLabel2: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
@@ -23,27 +24,25 @@ class loginController: UIViewController {
         
     }
     
-    func animatingWelcomeLabel() {
+    private func animatingWelcomeLabel() {
         
         //레이블의 투명도를 0으로 만들어 가리기
         welcomeLabel1.alpha = 0
         welcomeLabel2.alpha = 0
+        loginButton.alpha = 0
         
-        //일정 시간 후에 투명도를 1로 만들어 레이블 보이게1
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            UIView.animate(withDuration: 0.5) {
-                self.welcomeLabel1.alpha = 1
+        var times = 0.5
+        //일정 시간 후에 투명도를 1로 만들어 레이블 보이게
+        for element in [welcomeLabel1, welcomeLabel2, loginButton] {
+            DispatchQueue.main.asyncAfter(deadline: .now() + times) {
+                UIView.animate(withDuration: 0.5) {
+                    element?.alpha = 1
+                }
             }
-        }
-        
-        //일정 시간 후에 투명도를 1로 만들어 레이블 보이게2
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.3) {
-            UIView.animate(withDuration: 0.5) {
-                self.welcomeLabel2.alpha = 1
-            }
+            //0.8초 간격으로 구성요소들이 나타남
+            times += 0.8
         }
     }
-    
     
 }
 
